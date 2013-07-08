cd3e588e2a4fb0cd72acca848d32de5cf6fdec21
Andy Short <andynshort@gmail.com>
2013-07-08 00:13:52 -0700

    added tests for check

--- test/game_test.js
+++ test/game_test.js
@@ -17,5 +17,20 @@ new Test('piece movement', function() {
     assert (game.toMove === 'red');
     game.move('h1', 'g3');
     assert (game.toMove === 'black');
-  })
+  });
+
+  new Test('game knows when a general is in check', function() {
+    var game = new Game();
+    game.move('b3', 'b6');
+    game.move('a10', 'a9');
+    game.move('b6', 'e6');
+    assert (game.isCheck);
+  });
+
+  new Test('generals can check each other', function() {
+    var game = new Game();
+    game.move('e1', 'f2');
+    game.move('e10', 'f9');
+    assert (game.isCheck);
+  });
 });
