1a6738f353079261f2ede27469f149f66d1f6cd7
Andy Short <andynshort@gmail.com>
2013-07-15 14:08:36 -0700

    tested board returns nil for nonexistent square searches

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -11,4 +11,9 @@ describe Board do
   it "can find squares given their coordinates" do
     board.find(x: 0, y: 0).class.should eql(Square)
   end
+
+  it "finds nil for coordinates not on the board" do
+    board.find(x: 10, y: 0).should be(nil)
+    board.find(x: 0, y: 11).should be(nil)
+  end
 end
