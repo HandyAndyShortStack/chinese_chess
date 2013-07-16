316c7fa10c2660ead6813230bd2866d43e20df73
Andy Short <andynshort@gmail.com>
2013-07-16 10:17:33 -0700

    using metaprogramming instead of nested instance methods for graph construction

--- lib/board.rb
+++ lib/board.rb
@@ -23,10 +23,10 @@ private
   
   def make_squares
     @squares = {}
-    (0..9).each do |x|
-      (0..10).each do |y|
+    (0..8).each do |x|
+      @squares[x] = {}
+      (0..9).each do |y|
         square = Square.new x, y
-        @squares[x] = {} unless @squares.include? x
         @squares[x][y] = square
         if @squares[x - 1]
           left_square = @squares[x - 1][y]
@@ -34,9 +34,9 @@ private
           left_square.point right: square
         end
         if @squares[x][y - 1]
-          up_square = @squares[x][y - 1]
-          square.point up: up_square
-          up_square.point down: square
+          down_square = @squares[x][y - 1]
+          square.point down: down_square
+          down_square.point up: square
         end
       end
     end
