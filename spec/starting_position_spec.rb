4732c405f909da984d346922053e77309f6c8f97
Andy Short <andynshort@gmail.com>
2013-07-17 17:36:24 -0700

    added starting position

--- spec/starting_position_spec.rb
+++ spec/starting_position_spec.rb
@@ -5,7 +5,7 @@ describe StartingPosition do
   position = StartingPosition.new
 
   it "has properly placed generals" do
-    position.find(4, 0).class.should eql(General)
-    position.find(4, 10).class.should eql(General)
+    position.find(x: 4, y: 0).class.should eql(General)
+    position.find(x: 4, y: 9).class.should eql(General)
   end
 end
