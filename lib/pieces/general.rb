027d00e63bef739e8138a2bedb6fcc5184ed0734
Andy Short <andynshort@gmail.com>
2013-07-17 21:52:54 -0700

    kept general movement in palace

--- lib/pieces/general.rb
+++ lib/pieces/general.rb
@@ -5,7 +5,7 @@ class General < Piece
   def get_moves position
     directions.each do |direction|
       target_square = starting_square.send direction
-      next unless target_square
+      next unless target_square && in_palace?(target_square)
       target_piece = position.find target_square.coordinates
       next if target_piece && target_piece.color == @color
       add_move target_square.coordinates
