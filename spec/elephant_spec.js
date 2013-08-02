ee231e7756a139d4be97f795114e6dbf1ff90dda
Andy Short <andynshort@gmail.com>
2013-08-01 18:53:58 -0700

    implemented elephant movement

---
+++ spec/elephant_spec.js
@@ -0,0 +1,25 @@
+var Elephant = require('../src/elephant.js');
+var Position = require('../src/position.js');
+
+describe('Elephant', function() {
+  var elephant;
+  var position;
+
+  beforeEach(function() {
+    elephant = new Elephant('red');
+    position = new Position();
+  });
+
+  it('gives an accurate move list', function() {
+    position.import({
+      '4,2': elephant,
+      '2,4': new Elephant('red'),
+      '6,0': new Elephant('black')
+    });
+    var moveList = elephant.getMoves(position);
+
+    expect(moveList.indexOf('2,0')).not.toBe(-1);
+    expect(moveList.indexOf('6,0')).not.toBe(-1);
+    expect(moveList.indexOf('2,4')).toBe(-1);
+  });
+});
--- src/elephant.js
+++ src/elephant.js
@@ -3,6 +3,35 @@ var Piece = require('./piece.js');
 function Elephant(color) {
   var self = new Piece(color);
 
+  self.getMoves = function(position) {
+    var moves = [];
+    var directions = [
+      ['left', 'up'],
+      ['up', 'right'],
+      ['right', 'down'],
+      ['down', 'left']
+    ];
+    for (var dir_index = 0; dir_index < 4; dir_index += 1) {
+      var direction = directions[dir_index];
+      if (!this.square[direction[0]] || !this.square[direction[1]]) {
+        continue;
+      }
+      var blocking_square = this.square[direction[0]][direction[1]];
+      if (position[blocking_square.coordinates]) {
+        continue;
+      }
+      if (!blocking_square[direction[0]] || !blocking_square[direction[1]]) {
+        continue;
+      }
+      var target_square = blocking_square[direction[0]][direction[1]];
+      if (!position[target_square.coordinates] ||
+        position[target_square.coordinates].color !== this.color) {
+        moves.push(target_square.coordinates);
+      }
+    }
+    return moves;
+  };
+
   return self;
 }
 
