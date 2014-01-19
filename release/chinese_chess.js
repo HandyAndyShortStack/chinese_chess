05435d30cd1c29cb6066daedc332150802007ca3
Andy Short <andynshort@gmail.com>
2014-01-18 21:35:42 -0800

    fixed attacked piece isLegal bug

--- release/chinese_chess.js
+++ release/chinese_chess.js
@@ -227,18 +227,20 @@ function Game() {
   }
 
   function isLegal(startingCoordinates, endingCoordinates) {
-    var piece = position[startingCoordinates];
-    if (typeof piece.color === 'undefined') {
-      console.log(piece.color);
-    }
-    if (piece.color !== position.toMove) {
+    var movingPiece = position[startingCoordinates];
+    var attackedPiece = position[endingCoordinates];
+
+    if (movingPiece.color !== position.toMove) {
       return false;
     }
     move(startingCoordinates, endingCoordinates);
     toggleToMove();
     var legal = position.isCheck ? false : true;
     delete position[endingCoordinates];
-    position.place(piece, startingCoordinates);
+    position.place(movingPiece, startingCoordinates);
+    if (typeof attackedPiece !== 'undefined') {
+      position.place(attackedPiece, endingCoordinates);
+    }
     return legal;
   }
 
